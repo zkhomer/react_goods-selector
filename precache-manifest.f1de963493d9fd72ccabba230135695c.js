12f0c0e28926b775faac96c8ebcd1eb87387a9fe
Artur Bespalyy <zkhomers@gmail.com>
2020-10-28 15:25:04 +0200

    Updates

---
+++ precache-manifest.f1de963493d9fd72ccabba230135695c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e2c23966869a62d37356cb0d712e07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc8947a962565ba74393",
+    "url": "./static/css/main.c5a27ef0.chunk.css"
+  },
+  {
+    "revision": "b5ebe3227a36fe6a4ae7",
+    "url": "./static/js/2.c3d77166.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.c3d77166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8947a962565ba74393",
+    "url": "./static/js/main.0404a219.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
